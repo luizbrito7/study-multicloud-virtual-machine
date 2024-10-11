600af272455b198acaf01cf83f5801315b81dd6a
Kledson Basso <kledsonhugo@gmail.com>
2024-10-11 14:14:34 -0300

    Altera método de alocação do IP público para estático no módulo de VM do Azure

--- terraform/azure/modules/compute/vm.tf
+++ terraform/azure/modules/compute/vm.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "public-ip" {
   name                = "staticsite-vm-public-ip"
   location            = "${var.location}"
   resource_group_name = "${var.rg_name}"
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   domain_name_label   = "${var.fqdn}"
 }
 
